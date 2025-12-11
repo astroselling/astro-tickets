cbad4bc500f7dc6731fc26dd9a533b7d64a2bba0
Santi Evangelista <73905707+SantiEvangelista@users.noreply.github.com>
2025-12-11 16:52:29 -0300

    feat(web): Add health-check

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,8 @@ declare(strict_types=1);
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/health-check', function () {
+    return response()->json(['status' => 'ok']);
+});
+
 Route::get('/', fn (): View => view('welcome'));
